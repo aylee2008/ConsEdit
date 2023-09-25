2515397f71902eb0a7f60c768729849702bd5bf3
Ayoung Lee <aylee2020@snu.ac.kr>
2023-09-25 11:35:58 +0900

    feat: Modify edit_cli to process directories

--- edit_cli.py
+++ edit_cli.py
@@ -14,11 +14,14 @@ from einops import rearrange
 from omegaconf import OmegaConf
 from PIL import Image, ImageOps
 from torch import autocast
+import os
+import pickle
 
 sys.path.append("./stable_diffusion")
 
 from stable_diffusion.ldm.util import instantiate_from_config
 
+device = "cuda:0"
 
 class CFGDenoiser(nn.Module):
     def __init__(self, model):
@@ -59,7 +62,6 @@ def load_model_from_config(config, ckpt, vae_ckpt=None, verbose=False):
         print(u)
     return model
 
-
 def main():
     parser = ArgumentParser()
     parser.add_argument("--resolution", default=512, type=int)
@@ -77,51 +79,98 @@ def main():
 
     config = OmegaConf.load(args.config)
     model = load_model_from_config(config, args.ckpt, args.vae_ckpt)
-    model.eval().cuda()
+    model.eval().to(device)
     model_wrap = K.external.CompVisDenoiser(model)
     model_wrap_cfg = CFGDenoiser(model_wrap)
     null_token = model.get_learned_conditioning([""])
 
-    seed = random.randint(0, 100000) if args.seed is None else args.seed
-    input_image = Image.open(args.input).convert("RGB")
-    width, height = input_image.size
-    factor = args.resolution / max(width, height)
-    factor = math.ceil(min(width, height) * factor / 64) * 64 / min(width, height)
-    width = int((width * factor) // 64) * 64
-    height = int((height * factor) // 64) * 64
-    input_image = ImageOps.fit(input_image, (width, height), method=Image.Resampling.LANCZOS)
+    if not os.path.isdir(args.input):
+        seed = random.randint(0, 100000) if args.seed is None else args.seed
+        input_image = Image.open(args.input).convert("RGB")
+        width, height = input_image.size
+        factor = args.resolution / max(width, height)
+        factor = math.ceil(min(width, height) * factor / 64) * 64 / min(width, height)
+        width = int((width * factor) // 64) * 64
+        height = int((height * factor) // 64) * 64
+        input_image = ImageOps.fit(input_image, (width, height), method=Image.Resampling.LANCZOS)
 
-    if args.edit == "":
-        input_image.save(args.output)
-        return
+        if args.edit == "":
+            input_image.save(args.output)
+            return
 
-    with torch.no_grad(), autocast("cuda"), model.ema_scope():
-        cond = {}
-        cond["c_crossattn"] = [model.get_learned_conditioning([args.edit])]
-        input_image = 2 * torch.tensor(np.array(input_image)).float() / 255 - 1
-        input_image = rearrange(input_image, "h w c -> 1 c h w").to(model.device)
-        cond["c_concat"] = [model.encode_first_stage(input_image).mode()]
+        with torch.no_grad(), autocast("cuda"), model.ema_scope():
+            cond = {}
+            cond["c_crossattn"] = [model.get_learned_conditioning([args.edit])]
+            input_image = 2 * torch.tensor(np.array(input_image)).float() / 255 - 1
+            input_image = rearrange(input_image, "h w c -> 1 c h w").to(model.device)
+            cond["c_concat"] = [model.encode_first_stage(input_image).mode()]
 
-        uncond = {}
-        uncond["c_crossattn"] = [null_token]
-        uncond["c_concat"] = [torch.zeros_like(cond["c_concat"][0])]
+            uncond = {}
+            uncond["c_crossattn"] = [null_token]
+            uncond["c_concat"] = [torch.zeros_like(cond["c_concat"][0])]
 
-        sigmas = model_wrap.get_sigmas(args.steps)
+            sigmas = model_wrap.get_sigmas(args.steps)
 
-        extra_args = {
-            "cond": cond,
-            "uncond": uncond,
-            "text_cfg_scale": args.cfg_text,
-            "image_cfg_scale": args.cfg_image,
-        }
-        torch.manual_seed(seed)
-        z = torch.randn_like(cond["c_concat"][0]) * sigmas[0]
-        z = K.sampling.sample_euler_ancestral(model_wrap_cfg, z, sigmas, extra_args=extra_args)
-        x = model.decode_first_stage(z)
-        x = torch.clamp((x + 1.0) / 2.0, min=0.0, max=1.0)
-        x = 255.0 * rearrange(x, "1 c h w -> h w c")
-        edited_image = Image.fromarray(x.type(torch.uint8).cpu().numpy())
-    edited_image.save(args.output)
+            extra_args = {
+                "cond": cond,
+                "uncond": uncond,
+                "text_cfg_scale": args.cfg_text,
+                "image_cfg_scale": args.cfg_image,
+            }
+            torch.manual_seed(seed)
+            z = torch.randn_like(cond["c_concat"][0]) * sigmas[0]
+            z = K.sampling.sample_euler_ancestral(model_wrap_cfg, z, sigmas, extra_args=extra_args)
+            x = model.decode_first_stage(z)
+            x = torch.clamp((x + 1.0) / 2.0, min=0.0, max=1.0)
+            x = 255.0 * rearrange(x, "1 c h w -> h w c")
+            edited_image = Image.fromarray(x.type(torch.uint8).cpu().numpy())
+        edited_image.save(args.output)
+
+    else:
+        with open(args.edit, "rb") as f:
+            edit_dicts = pickle.load(f)
+        for inp_file in sorted(os.listdir(args.input)):
+            seed = random.randint(0, 100000) if args.seed is None else args.seed
+            input_image = Image.open(args.input + inp_file).convert("RGB")
+            width, height = input_image.size
+            factor = args.resolution / max(width, height)
+            factor = math.ceil(min(width, height) * factor / 64) * 64 / min(width, height)
+            width = int((width * factor) // 64) * 64
+            height = int((height * factor) // 64) * 64
+            input_image = ImageOps.fit(input_image, (width, height), method=Image.Resampling.LANCZOS)
+
+            if edit_dicts[inp_file] == "":
+                input_image.save(args.output)
+                return
+
+            with torch.no_grad(), autocast("cuda"), model.ema_scope():
+                cond = {}
+                cond["c_crossattn"] = [model.get_learned_conditioning([edit_dicts[inp_file]])]
+                input_image = 2 * torch.tensor(np.array(input_image)).float() / 255 - 1
+                input_image = rearrange(input_image, "h w c -> 1 c h w").to(model.device)
+                cond["c_concat"] = [model.encode_first_stage(input_image).mode()]
+
+                uncond = {}
+                uncond["c_crossattn"] = [null_token]
+                uncond["c_concat"] = [torch.zeros_like(cond["c_concat"][0])]
+
+                sigmas = model_wrap.get_sigmas(args.steps)
+
+                extra_args = {
+                    "cond": cond,
+                    "uncond": uncond,
+                    "text_cfg_scale": args.cfg_text,
+                    "image_cfg_scale": args.cfg_image,
+                }
+                torch.manual_seed(seed)
+                z = torch.randn_like(cond["c_concat"][0]) * sigmas[0]
+                z = K.sampling.sample_euler_ancestral(model_wrap_cfg, z, sigmas, extra_args=extra_args)
+                x = model.decode_first_stage(z)
+                x = torch.clamp((x + 1.0) / 2.0, min=0.0, max=1.0)
+                x = 255.0 * rearrange(x, "1 c h w -> h w c")
+                edited_image = Image.fromarray(x.type(torch.uint8).cpu().numpy())
+            os.makedirs(args.output + f"scale_{args.cfg_image}/", exist_ok=True)
+            edited_image.save(args.output + f"scale_{args.cfg_image}/" + inp_file)
 
 
 if __name__ == "__main__":
