802f73ddf996c45003f5964cb1a02c701d900292
Ayoung Lee <aylee2020@snu.ac.kr>
2024-07-26 19:33:41 +0900

    feat: Update download_checkpoints.sh

--- scripts/download_checkpoints.sh
+++ scripts/download_checkpoints.sh
@@ -4,5 +4,5 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p $SCRIPT_DIR/../checkpoints
 
-wget "https://mysnu-my.sharepoint.com/:u:/g/personal/aylee2020_seoul_ac_kr/ETo38lq2I3BBpJ_YkKuR5JUBiqBqCUHd5xLrjADbbqh10w?e=3CxVt1&download=1" -O $SCRIPT_DIR/../checkpoints/epoch=000032-step=000019999.ckpt
-wget "https://mysnu-my.sharepoint.com/:u:/g/personal/aylee2020_seoul_ac_kr/EXUsCsfbbwtIifwfc7MgYsYB8KxYd1XDJlDGUQoZ-6fRLQ?e=QZyseE&download=1" -O $SCRIPT_DIR/../checkpoints/instruct-pix2pix-00-22000.ckpt
+wget "https://drive.usercontent.google.com/download?id=1SWF4VIgjYt91iZGqfk6zZFauyLG-tzYz&export=download&authuser=0&confirm=t" -O $SCRIPT_DIR/../checkpoints/epoch=000032-step=000019999.ckpt
+wget "https://drive.usercontent.google.com/download?id=1dAbCi1vfvqBihPh8eTVT_XtiLhzxwI3K&export=download&authuser=0&confirm=t" -O $SCRIPT_DIR/../checkpoints/instruct-pix2pix-00-22000.ckpt
