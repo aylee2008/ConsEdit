043fddcaa88e43c9e51f546b75580ff21df44d5c
Ayoung Lee <aylee2020@snu.ac.kr>
2024-07-26 19:27:57 +0900

    feat: Update download_data.sh

--- scripts/download_data.sh
+++ scripts/download_data.sh
@@ -5,7 +5,7 @@ SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 mkdir -p $SCRIPT_DIR/../data/
 
-wget "https://mysnu-my.sharepoint.com/:u:/g/personal/aylee2020_seoul_ac_kr/ER4VmkwYok1NujrktVo6F38BWXRROu6-1AKr2bODJqnCLA?e=6feTbQ&download=1" -O $SCRIPT_DIR/../data/img_metrics.zip
-wget "https://mysnu-my.sharepoint.com/:u:/g/personal/aylee2020_seoul_ac_kr/EUf-B6Np2JhEvyBjdue4QgQBxUTGlLgLz4qgnKmI99az3g?e=KxwYD7&download=1" -O $SCRIPT_DIR/../data/obj_metrics.zip
+wget "https://drive.usercontent.google.com/download?id=1x7xRBmFEt1xa75-Lb23kNwCqZN4CVR7Z&export=download&authuser=0&confirm=t" -O $SCRIPT_DIR/../data/img_metrics.zip
+wget "https://drive.usercontent.google.com/download?id=1k0ocXeuUWSvvRnANiS2pKPbTjkxcISdf&export=download&authuser=0&confirm=t" -O $SCRIPT_DIR/../data/obj_metrics.zip
 
 unzip $SCRIPT_DIR/../data/\*.zip -d $SCRIPT_DIR/../data/$1/
